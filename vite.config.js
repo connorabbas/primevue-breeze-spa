2ec727281391de6b57e16636a46d6cbc63b14c22
Connor Abbas <abbasconnor@gmail.com>
2024-07-30 00:02:08 +0000

    port config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const devPort = parseInt(env.VITE_PORT);
+
+    return defineConfig({
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
-    server: {
-        host: true,
-        port: 8000, // adjust as needed, match docker-compose.yml port values, same for below
-        watch: {
-            usePolling: true,
+        server: {
+            host: true,
+            port: devPort,
+            watch: {
+                usePolling: true,
+            },
         },
-    },
-    preview: {
-        port: 8000,
-    },
-});
+        preview: {
+            port: devPort,
+        },
+    });
+};
