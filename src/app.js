a346f4ff9120531c4d00c375f7f4d6b4f471d212
Connor Abbas <abbasconnor@gmail.com>
2024-08-14 01:34:58 +0000

    consolidate theme file, misc

--- src/app.js
+++ src/app.js
@@ -2,36 +2,27 @@ import './assets/css/app.css';
 import 'nprogress/nprogress.css';
 import 'primeicons/primeicons.css';
 
-import customizedThemePreset from '@/modules/theme-preset';
+import customThemePreset from '../theme-preset.js';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import router from "./router";
+import router from './router';
 
-import App from "./App.vue";
-import PrimeVue from "primevue/config";
-import ToastService from "primevue/toastservice";
-import InputText from "primevue/inputtext";
-import Button from "primevue/button";
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 
 const app = createApp(App);
 
 app.use(createPinia())
     .use(router)
     .use(PrimeVue, {
-        theme: {
-            preset: customizedThemePreset,
-            options: {
-                darkModeSelector: '.dark-mode',
-                cssLayer: {
-                    name: 'primevue',
-                    order: 'tailwind-base, primevue, tailwind-utilities',
-                },
-            },
-        },
+        theme: customThemePreset,
     })
     .use(ToastService)
-    .component("InputText", InputText)
-    .component("Button", Button)
-    .mount("#app");
+    .component('InputText', InputText)
+    .component('Button', Button)
+    .mount('#app');
